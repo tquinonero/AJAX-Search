384e55402cd58dd66b40bd621a2f7e07de1ae9f6
tquinonero <152458286+tquinonero@users.noreply.github.com>
2024-09-19 23:52:18 +0200

    Update ajax-search.js
    Added proper aria-roles and made search results keyboard toggable

--- js/ajax-search.js
+++ js/ajax-search.js
@@ -2,6 +2,10 @@ jQuery(document).ready(function($) {
     var searchInput = $('#ajax-search-input');
     var searchResults = $('#ajax-search-results');
     var timer;
+    var currentFocus = -1;
+
+    searchInput.attr('aria-autocomplete', 'list');
+    searchResults.attr('role', 'listbox');
 
     searchInput.on('input', function() {
         clearTimeout(timer);
@@ -15,6 +19,35 @@ jQuery(document).ready(function($) {
             searchResults.empty();
         }
     });
+
+    searchInput.on('keydown', function(e) {
+        var items = searchResults.find('li');
+        if (e.keyCode == 40) { // down arrow
+            currentFocus++;
+            addActive(items);
+        } else if (e.keyCode == 38) { // up arrow
+            currentFocus--;
+            addActive(items);
+        } else if (e.keyCode == 13) { // enter
+            e.preventDefault();
+            if (currentFocus > -1) {
+                if (items.length) items[currentFocus].click();
+            }
+        }
+    });
+
+    function addActive(items) {
+        if (!items.length) return false;
+        removeActive(items);
+        if (currentFocus >= items.length) currentFocus = 0;
+        if (currentFocus < 0) currentFocus = (items.length - 1);
+        $(items[currentFocus]).addClass('active').attr('aria-selected', 'true');
+        searchInput.attr('aria-activedescendant', $(items[currentFocus]).attr('id'));
+    }
+
+    function removeActive(items) {
+        items.removeClass('active').attr('aria-selected', 'false');
+    }
 
     function performSearch(query) {
         $.ajax({
@@ -34,12 +67,16 @@ jQuery(document).ready(function($) {
     }
 
     function displayResults(results) {
-        searchResults.empty();
+        searchResults.empty().attr('aria-hidden', 'false');
 
         if (results.length > 0) {
             var ul = $('<ul>');
             $.each(results, function(index, item) {
-                var li = $('<li>');
+                var li = $('<li>', {
+                    id: 'search-result-' + index,
+                    role: 'option',
+                    'aria-selected': 'false'
+                });
                 var link = $('<a>', {
                     href: item.permalink,
                     text: item.title,
@@ -52,5 +89,6 @@ jQuery(document).ready(function($) {
         } else {
             searchResults.append('<p>No results found.</p>');
         }
+        currentFocus = -1;
     }
 });
